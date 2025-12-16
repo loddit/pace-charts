73547badc5d54a6114de2995b94da5b06a07dcc2
Loddit Li <loddit@gmail.com>
2025-12-16 09:27:17 +0800

    add umami cloud code

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.PROD) {
+  const websiteId = import.meta.env.VITE_UMAMI_CLOUD_WEBSITE_ID
+
+  // Function to dynamically insert Umami analytics script
+  const insertUmamiScript = () => {
+    if (!websiteId) {
+      console.error('Missing Umami website ID')
+      return
+    }
+    const script = document.createElement('script')
+    script.defer = true
+    script.src = 'https://cloud.umami.is/script.js'
+    script.setAttribute('data-website-id', websiteId)
+    document.head.appendChild(script)
+  }
+
+  // Insert the Umami script only in production environment
+  insertUmamiScript()
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
